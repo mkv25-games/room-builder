ae95f493084bcee89d6792e860e12d6c8db606ff
Markavian <Markavian@8e873224-7694-9d4a-bd94-d9488b977d6f>
2014-02-16 11:15:05 +0000

    Still messing around trying to get rooms to play nicely.
    git-svn-id: https://blackrock/svn/room-builder/trunk@6 8e873224-7694-9d4a-bd94-d9488b977d6f

--- code/src/net/mkv25/room/planner/Floorplan.hx
+++ code/src/net/mkv25/room/planner/Floorplan.hx
@@ -1,4 +1,5 @@
 package net.mkv25.room.planner;
+import flash.geom.Rectangle;
 
 class Floorplan
 {
@@ -6,9 +7,22 @@ class Floorplan
 	public var height:Int;
 	public var rooms:List<Room>;
 
+	private var filled:Bool;
+	
 	public function new() 
 	{
 		rooms = new List<Room>();
+	}
+	
+	public function addRoom(room:Room):Bool
+	{
+		if (spaceFor(room))
+		{
+			rooms.add(room);
+			return true;
+		}
+		
+		return false;
 	}
 	
 	public function removeAllRooms():Void
@@ -17,5 +31,27 @@ class Floorplan
 		{
 			rooms.pop();
 		}
+	}
+	
+	public function hasSpace():Bool
+	{
+		return !filled;
+	}
+	
+	public function spaceFor(check:Room):Bool
+	{
+		var r1:Rectangle = check.dimensions();
+		var r2:Rectangle;
+		
+		for (room in rooms)
+		{
+			r2 = room.dimensions();
+			if (r1.intersects(r2))
+			{
+				return false;
+			}
+		}
+		
+		return true;
 	}
 }
